80bf79b274b8efb110c1b6df241295801892eaf9
KronoSeptic <96399896+KronoSeptic15@users.noreply.github.com>
2022-03-21 19:14:50 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace Piss{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin{
+		bool inRoom;
+		void Update(){
+			if (inRoom){
+				GameObject cherryBlossoms = GameObject.Find("Cherry Blossoms (2)");
+				if (cherryBlossoms != null){
+					Destroy(cherryBlossoms);
+				}
+			}
+		}
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode){
+			inRoom = true;
+		}
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode){
+			inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Piss
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.krono.gorillatag.piss";
+		public const string Name = "Piss";
+		public const string Version = "1.0.0";
+	}
+}
